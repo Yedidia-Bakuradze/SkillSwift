755d15787c14efb2482f5693d9aa89c49a51080f
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-05-21 12:29:41 +0300

    User model has been implemented

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+/**
+ * Makes pre-saving processes
+ * It hashes the entered password before storing it, only if the password field has been modifed
+ *  */
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
