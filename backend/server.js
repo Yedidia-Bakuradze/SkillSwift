41e7010b27f1c3cddf390f3e70bb48b98149105c
Yedidia Bakuradze <yedidiadev@gmail.com>
2025-05-21 12:08:57 +0300

    Backend server is online with test API endpoint

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/test", (req, res) => {
+  res.send({ message: "Server Online" });
+});
+
+app.listen(port, () => console.log(`Server is online on port: ${port}`));
